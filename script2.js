5403386cba8d1c5f6b7fc73e36402ac889457264
Deep Singh <deepsingh25244@gmail.com>
2023-07-09 22:37:11 +0530

    second  commit

--- script2.js
+++ script2.js
@@ -12,4 +12,33 @@ const firebaseConfig = {
   const auth = firebase.auth()
   const database = firebase.database()
 
-  function 
+  function login(){
+    email =document.getElementById("email").value
+    password = document.getElementById("password").value
+
+      // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!')
+    return
+    // Don't continue running the code
+  }
+    auth.signinWithEmailAndPassword(email, password)
+    .then(function() {
+        var user = auth.currentUser
+        var database_ref = database.ref()
+
+        var user_data = {
+            last_login : Date.now()
+        }
+
+        database_ref.child('users/' + user.uid).update(user_data)
+        alert('Logged In ')
+
+    })
+    .catch(function(error){
+        var error_code = error.error_code
+        var error_message = error.message
+
+        alert(error_message)
+    })
+  }
